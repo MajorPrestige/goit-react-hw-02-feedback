48812e86ba4ba61bd812e88852376fbba97438fa
Denis Dashkevich <majorprestige99@gmail.com>
2022-08-15 14:31:51 +0300

    added json EsLint

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,5 @@
 import Statistics from 'components/Statisctics/Statistics.jsx';
 
 export const App = () => {
-  return (
-    <Statistics good={0} neutral={0} bad={0} total={0} positivePercentage={0} />
-  );
+  return <Statistics />;
 };
